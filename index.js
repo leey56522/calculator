f6f232d702ff6aa7d14922ac522d3c0cd5e57bc6
Lucia <pongu80@gmail.com>
2021-10-18 17:17:53 -0500

    Created calculator structure with css

---
+++ index.js
@@ -0,0 +1,27 @@
+const add = function(a, b) {
+    return a + b;
+  };
+  
+const subtract = function(a, b) {
+    return a - b;
+  };
+
+const multiply = function(a, b) {
+    return a * b;
+  };
+  
+const divide = function(a, b) {
+    return a / b;
+  };
+
+const operate = function(operator, a, b) {
+    if (operator == 'add') {
+        return add(a, b);
+    } else if (operator == 'subtract') {
+        return subtract(a, b);
+    } else if (operator == 'multiply') {
+        return multiply(a, b);
+    } else if (operator == 'divide') {
+        return divide(a, b);
+    }
+}
